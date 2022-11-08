9c3027bdd99ccdfaf365fd8ffd797ff49af1bc38
allisteraniceto <allisteraniceto@gmail.com>
2022-11-08 09:06:31 -0800

    changed to a cpp file and added main comments

---
+++ huffman_prep.cpp
@@ -0,0 +1,79 @@
+//Adaptive Huffman Prep
+
+//./huff.exe encode alpha.txt message.txt.encoded - message.txt.encoded
+//./huff.exe decode alpha.txt message.txt.decoded - message.txt.decoded
+
+class AdaptiveHuffman{
+    
+};
+
+int main(int argc, char const *argv[]){
+    //check for correct # of arguments
+    //if too little OR too many arguments, prompt user to enter again
+
+    //identify commands given
+    //if encode command
+    //else if decode command
+    //else incorrect command
+
+    //get alphabet from alphabe.txt
+    //open alphabet file
+    //read single line into string variable (alphabet.txt only has one string)
+    //*remember: close file
+
+    //get message from message.txt
+    //open message file
+    //read each line and append to a string
+    //*remember: close file
+
+    //create AdaptiveHuffman object
+
+
+}
+
+/*max heap: 
+-largest value (# of characters) on top
+-create array of characters (0's or 1's) from right to left
+-nodes have counter and character attributes
+
+0 node will always be at the bottom
+-0 node contains characters that havent been used yet
+-backbone like structure
+-when you encounter zero node, next 8 bits are ascii value of character
+
+how array (thread) is enconded:
+-path to 0 node or a character
+    0 left, 1 right
+-if (first encounter)
+    path to 0 node, then ascii value of character 
+-else if (encounter again)
+    path to character, then 1
+
+huffman tree:
+    encoding
+    -when encountering a new character, pull character from zero node and create 2 new nodes on top of 0 node
+        -first encounter will be ascii value in binary
+        -new nodes will have value of 1 by default
+    -sibling property
+        if (leader is parent of count)
+            just increment parent node
+        else if (leader is NOT parent)
+            swap position with sibling
+    -when a character is encountered again, increment character's parent count by 1
+    -after every encounter, increment parent count by 1 up along the tree.
+
+    decoding
+    -start with zero node
+    -first few bits are the path to the root node
+    -when you encounter 0 node, next 8 bits is ascii value character
+    -make 2 new nodes on top with default value one 1, output character
+        if new node
+            path to root node starts at 0
+        else if character is encountered again
+            path to character
+    -after every character decode, check sibling property for any violations
+        if (sibling violation)
+            swap positions with the leader
+        else if (NO sibling violations)
+            increment parent count by 1, up along the tree 
+*/
